e216af9341465e6aec076bfc277f4bb3fcc8d816
chanoto89 <csoto@mail.usf.edu>
2017-03-11 07:12:40 -0700

    Added basic server.js content to get app running

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var server = require('http').createServer(app);
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+//Angular Build Directory
+var distDir = __dirname + '/dist/';
+app.use(express.static(distDir));
+
+// angular 2 spa routing catch-all
+// angular 2 will handle 404 errors
+app.get('*', function(req, res) {
+    res.sendFile(distDir + 'index.html');
+});
+
+server.listen(port, function() {
+   console.log("App now running on port", port); 
+});
