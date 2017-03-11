6c79d83a7abbf2022b55bf545eb9e65f8d2c0eea
chanoto89 <csoto@mail.usf.edu>
2017-03-11 07:05:16 -0700

    Restructured the angular components. Also added bootstrap to the project. Getting the server.js file ready

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+
+gulp.task('migrate-bootstrap-files', function() {
+    gulp.src('node_modules/bootstrap/dist/js/**').pipe(gulp.dest('dist/js'));
+    gulp.src('node_modules/bootstrap/dist/fonts/**').pipe(gulp.dest('dist/fonts'));
+    gulp.src('images/**').pipe(gulp.dest('dist/images'));
+    return gulp.src('css/bootswatch-yeti-theme.min.css').pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('dev', function() {
+    return gulp.src('node_modules/bootstrap/dist/js/**').pipe(gulp.dest('js'));
+});
+
+gulp.task('default', ['migrate-bootstrap-files'], function() {})
